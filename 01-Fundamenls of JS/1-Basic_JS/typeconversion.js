465e20c3371d43ea038ff1eed53e7499e3a5a534
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-01-22 21:13:51 +0600

    TypeConversion in JS

---
+++ 01-Fundamenls of JS/1-Basic_JS/typeconversion.js
@@ -0,0 +1,25 @@
+/* Type Conversion */
+
+
+/* Converting String to Numbers */
+let pi = "3.1416";
+
+
+// console.log(typeof(pi),  typeof(Number(pi)), Number(""), Number(" "), Number('S')  );
+
+// console.log(pi, parseFloat(pi),  parseInt(pi));
+
+
+/* Converting  Number to String*/
+let result =  String(1234+"23");
+// console.log(result,  1234+"23");
+
+let x = 1.3455464;
+// console.log(x, x.toString(), );
+
+
+/* Converting Boolean to String */
+console.log(false, String(false), false.toString());
+
+/* Converting Boolean to Numbers */
+console.log(Number(false), Number(true));
