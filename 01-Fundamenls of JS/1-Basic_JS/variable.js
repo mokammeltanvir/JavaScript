bde4ae2638040b9526e6af9c6e3c14ed669e76fa
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-01-22 20:55:58 +0600

    variable in JS

---
+++ 01-Fundamenls of JS/1-Basic_JS/variable.js
@@ -0,0 +1,26 @@
+/* Variables 
+
+1. var
+2. let
+3. const
+
+*/
+/*          Global Scope      Block Scope */
+/*  Var ---     YES              NO
+    Let ---     NO               YES
+*/
+
+var name = "35";
+// var age = 35;
+name = 35;
+name = "C"
+
+
+
+let intro = "JavaScript Fundamentals"
+intro = 500
+
+
+const pi = 3.1416;
+pi = 4.5;
+console.log(intro, pi);
