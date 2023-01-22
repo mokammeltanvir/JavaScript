f908c58a364ef76d78364532fbb6c84e2748d1ec
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-01-22 21:18:11 +0600

    Math oject and number methods in JS

---
+++ 01-Fundamenls of JS/1-Basic_JS/mathobjectandnumber.js
@@ -0,0 +1,24 @@
+/* Math Object */
+
+// let x = -4.9;
+// let y = 2;
+
+/*
+round
+ceil
+floor
+trunc
+
+
+Sign
+Pow X^y
+Sqrt
+abs
+min 
+max
+
+random
+
+*/
+
+console.log(Math.round (Math.random()*10000));   /// 0 - 1   OTP generation
