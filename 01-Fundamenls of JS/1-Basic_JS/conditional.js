67766ae102673f699fb762f665b9a22ac8d08aaa
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-01-23 20:35:18 +0600

    Conditional statement in js

---
+++ 01-Fundamenls of JS/1-Basic_JS/conditional.js
@@ -0,0 +1,36 @@
+/* Conditionals */
+
+/*
+
+if 
+else 
+else if
+switch
+
+if(condition){
+    /// action
+}
+*/
+
+// let age = 75;
+
+// if(age>18 && age<60){
+//     console.log("You can have a NID Card");
+// }else if(age>60 && age<90){
+//     console.log("Please consult with NID Officer");
+// }else{
+//     console.log("Your are a child");
+// }
+
+let color = 'blue';
+
+switch(color){
+    case 'red': 
+        console.log('This color is red');
+        break;
+    case 'green':
+        console.log('This color is green');
+        break
+    default: 
+        console.log('Unknown color!!');
+}
